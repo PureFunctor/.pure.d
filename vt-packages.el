2628f73bf9173c981646b39fd39a84e13d3fa122
PureFunctor <purefunctor@gmail.com>
2022-01-21 00:34:52 +0800

    Update theme

--- vt-packages.el
+++ vt-packages.el
@@ -8,7 +8,7 @@
 
 (setup (:straight doom-themes)
   (:require doom-themes)
-  (load-theme 'doom-vibrant t)
+  (load-theme 'doom-horizon t)
   (disable-theme 'wombat))
 
 (setup (:straight diminish))
@@ -24,36 +24,29 @@
   (dashboard-setup-startup-hook))
 
 (setup (:straight consult)
-  (:defer)
   (:global "C-s" consult-line
            "M-y" consult-yank-pop))
 
-(setup (:require vt-project)
-  (:with-map project-prefix-map
-    (:bind "t" vt/project-ansi-term)))
+(setup (:straight project)
+  (setup (:require vt-project)
+    (:with-map project-prefix-map
+      (:bind "t" vt/project-ansi-term))))
 
 (setup (:straight perspective)
-  (:defer)
   (:option persp-mode-prefix-key (kbd "M-i")
            persp-initial-frame-name "Main")
-  (:when-loaded
-    (persp-mode)))
+  (persp-mode))
 
 (setup (:straight ace-window)
-  (:defer)
   (:option aw-dispatch-always t
            aw-display-mode-overlay nil
            aw-keys '(?a ?s ?d ?f ?q ?w ?e ?r))
   (:global "M-o" ace-window)
-  (:when-loaded
-    (ace-window-display-mode)))
+  (ace-window-display-mode))
 
 (setup (:straight which-key)
-
-  (:defer)
   (:diminish t)
-  (:when-loaded
-    (which-key-mode)))
+  (which-key-mode))
 
 (setup (:require eldoc)
   (:hook (apply-partially 'diminish 'eldoc-mode)))
@@ -65,75 +58,79 @@
     (set-face-attribute 'font-lock-keyword-face nil :slant 'italic)))
 
 (setup (:straight selectrum)
-  (:defer)
-  (:when-loaded
-    (selectrum-mode +1)))
+  (:option selectrum-highlight-candidates-function #'orderless-highlight-matches)
+  (selectrum-mode +1))
 
 (setup (:straight orderless)
-  (:defer)
   (:option completion-styles '(orderless)
            completion-category-defaults nil
-           completion-category-overrides '((file (styles partial-completion)))))
+           completion-category-overrides '((file (styles partial-completion)))
+           orderless-skip-highlighting (lambda () selectrum-is-active)))
 
 (setup (:straight marginalia)
-  (:defer)
-  (:when-loaded
-    (marginalia-mode)))
+  (marginalia-mode)
+  (:with-map minibuffer-local-map
+    (:bind "M-a" marginalia-cycle)))
 
 (setup (:straight diredfl)
-  (:defer)
   (:hook-into dired-mode-hook))
 
-(setup org
-  (:defer)
+(setup (:straight org)
   (:option org-directory "~/PureFunctor/Org/Agenda"
            org-agenda-files (list org-directory)
-           org-hide-emphases-markers t
+           org-hide-emphasis-markers t
            org-edit-src-content-indentation 0
            fill-column 100)
-  (:hook org-indent-mode))
+  (:when-loaded
+    (add-to-list 'org-src-lang-modes '("purescript" . purs)))
+  (setup (:require vt-org-face-mode))
+  (:hook org-indent-mode vt/org-face-mode
+         (lambda () (fringe-mode 8))))
 
 (setup (:straight org-superstar)
-  (:defer)
-  (:option org-superstar-leading-bullet " ")
+  (:option org-superstar-leading-bullet " "
+           org-superstar-headline-bullets-list '(?▶ ?▷))
   (:hook-into org-mode-hook))
 
-(setup (:straight visual-fill-column)
-  (:defer)
-  (:option visual-fill-column-width 120
-           visual-fill-column-center-text t)
-  (:hook-into org-mode-hook))
+(setup (:straight haskell-mode)
+  (:unbind "M-[" "M-]")
+  (:bind "M-]" 'haskell-goto-next-error
+         "M-[" 'haskell-goto-prev-error)
+  (:hook interactive-haskell-mode)
+  (setup (:require haskell-interactive-mode)
+    (:option haskell-process-use-presentation-mode t)
+    (:unbind "M-[" "M-]")
+    (:bind "M-]" 'haskell-interactive-mode-error-forward
+           "M-[" 'haskell-interactive-mode-error-backward)))
 
-(setup (:straight
-        (purs-mode :type git
-                   :flavor melpa
-                   :files (:defaults "purs-mode-pkg.el")
-                   :host github
-                   :repo "PureFunctor/purs-mode")))
+(setup (:straight popper)
+  (:bind "M-`" 'popper-cycle)
+  (:option popper-reference-buffers
+           '("\\*Messages\\*"
+             "Output\\*$"
+             "\\*Async Shell Command\\*"
+             help-mode
+             compilation-mode
+             haskell-interactive-mode))
+  (popper-mode +1)
+  (popper-echo-mode +1))
 
 (setup (:straight org-roam)
-  (:defer)
   (:option org-roam-directory "~/PureFunctor/Org/Roam"
            org-roam-completion-everywhere t
            org-roam-v2-ack t)
   (:global "C-c n l" org-roam-buffer-toggle
            "C-c n f" vt/org-roam-node-find
            "C-c n i" vt/org-roam-node-insert)
-  (:when-loaded
-    (:require vt-org-roam-utils)
-    (org-roam-db-autosync-mode)))
+  (:require vt-org-roam-utils)
+  (org-roam-db-autosync-mode))
 
-(setup (:straight wakatime-mode)
-  (:defer)
-  (:diminish t)
-  (:option wakatime-api-key (with-temp-buffer
-                              (insert-file-contents "~/Secrets/wakatime-api-key")
-                              (string-trim (buffer-string))))
-  (:when-loaded
-    (global-wakatime-mode)))
+;; (setup (:straight wakatime-mode)
+;;   (:diminish t)
+;;   (:option wakatime-api-key user-wakatime-key)
+;;   (global-wakatime-mode))
 
 (setup (:straight ws-butler)
-  (:defer)
   (:diminish t)
   (:hook-into prog-mode))
 
@@ -142,13 +139,35 @@
   (:option git-commit-summary-max-length 50)
   (:option transient-default-level 5))
 
+(setup (:straight forge))
+
 (setup (:straight flycheck)
   (:global "M-p" flycheck-buffer
            "M-[" flycheck-previous-error
            "M-]" flycheck-next-error)
-  (:option flycheck-check-syntax-automatically nil)
-  (:hook-into purs-mode))
+  (:option flycheck-check-syntax-automatically '(save))
+  (setup (:require flycheck-spago)))
 
-(setup (:require flycheck-spago))
+(setup (:straight lsp-mode)
+  (:option lsp-keymap-prefix "M-l"
+           lsp-lens-enable nil)
+  (:when-loaded
+    (add-to-list 'lsp-language-id-configuration '(purs-mode . "purescript"))
+    (lsp-register-client
+     (make-lsp-client
+      :new-connection (lsp-stdio-connection '("purescript-language-server" "--stdio"))
+      :activation-fn (lsp-activate-on "purescript")
+      :server-id 'purescript-language-server))))
+
+(setup (:straight tuareg)
+  (:hook lsp flycheck-mode))
+
+(add-to-list 'load-path "~/Vitriol/purs-mode")
+(setup (:require purs-mode)
+  (:hook lsp flycheck-mode purs-indentation-mode)
+  (:bind "M-g M-n" flymake-goto-next-error
+         "M-g M-p" flymake-goto-prev-error))
+
+(setup (:straight markdown-mode))
 
 (provide 'vt-packages)
