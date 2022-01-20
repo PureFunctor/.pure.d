d6c3b7167112a1bdffd5120ffbb8b5e8afc368ae
PureFunctor <purefunctor@gmail.com>
2022-01-21 00:35:06 +0800

    Update shell to fish

--- lisp/vt-project.el
+++ lisp/vt-project.el
@@ -2,6 +2,6 @@
   "Start `ansi-term' in the current project's root directory."
   (interactive)
   (let* ((default-directory (project-root (project-current t))))
-    (ansi-term "/usr/bin/zsh")))
+    (ansi-term "/usr/bin/fish")))
 
 (provide 'vt-project)
